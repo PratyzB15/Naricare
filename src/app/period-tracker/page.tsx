57472104b219f3b1be8adcddd18e1c69171e0cc7
Suparna Banerjee <suparnabanerjee444@gmail.com>
2025-07-30 15:38:05 +0000

    in cycle calender when user uses first time first give option to select

--- src/app/period-tracker/page.tsx
+++ src/app/period-tracker/page.tsx
@@ -1,9 +1,13 @@
 import { PeriodTracker } from '@/components/her-health/PeriodTracker';
+import { Header } from '@/components/her-health/Header';
 
 export default function PeriodTrackerPage() {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
         <PeriodTracker />
+      </main>
     </div>
   );
 }
