f9bbfcffd26bdc4ff446c7a24e227808b482b259
Suparna Banerjee <suparnabanerjee444@gmail.com>
2025-08-10 06:55:14 +0000

    Runtime Error
    Error: Invalid src prop (https://storage.googleapis.com/g

--- src/app/sex-education/page.tsx
+++ src/app/sex-education/page.tsx
@@ -167,7 +167,7 @@ export default function SexEducationPage() {
                 <AccordionItem value={section.value} key={section.title}>
                   <AccordionTrigger className="text-lg font-semibold">
                     <div className="flex items-center gap-3">
-                        <section.icon className="h-5 w-5 text-primary" />
+                        <section.icon className="h-5 w-5 text-pink-500" />
                         {section.title}
                     </div>
                   </AccordionTrigger>
